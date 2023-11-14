e27389d29a837e3c7b6d91feb2af64f96f4eca38
samirfaritov <samirfaritov6@gmail.com>
2023-11-14 20:01:24 +0500

    Taymer

--- main.js
+++ main.js
@@ -21,41 +21,11 @@ let b = 0
 
 item.addEventListener('click', () => {
     interval = setInterval(() => {
-        mm++
-        if (mm <= 9) {
-            millisecound.innerHTML = `0${mm}`
-        } else {
-            millisecound.innerHTML = mm
-        }
-        if (mm <= 9) {
-            millisecound.innerHTML = `0${mm}`
-        } else {
-            millisecound.innerHTML = mm
-        }
-        
-        if (mm == 100) {
-            s++
-            secound.innerHTML = s
-            mm = 0
-            millisecound.innerHTML = mm
-        }
-        
-        if (s <= 9) {
-            secound.innerHTML = `0${s}`
-        } else {
-            secound.innerHTML = s
-        }
-        if (m <= 9) {
-            minute.innerHTML = `0${m}`
-        } else {
-            minute.innerHTML = m
-        }
-        
-        if (s == 60) {
-            m++
-            minute.innerHTML = m
-            s = 0
-            secound.innerHTML = s
+        mm--
+        millisecound.innerHTML = getZero(mm)
+
+        if (mm < 0) {
+            s = 59
         }
     }, 10)
 })
@@ -84,4 +54,57 @@ box.addEventListener('click', () => {
         } else {
             minute.innerHTML = m
     }
+})
+
+
+
+function getZero(num) {
+    if (num <= 9) {
+        return `0${num}`
+    } else {
+        return num
+    }
+}
+
+
+
+
+box.addEventListener('click', () => {
+    m++
+    if (m <= 59) {
+        minute.innerHTML = getZero(m)
+    } else{
+        m = 0
+        minute.innerHTML = getZero(m)
+    }
+})
+box2.addEventListener('click', () => {
+    m--
+    if (m == 0) {
+        
+    }
+})
+box1.addEventListener('click', () => {
+    s++
+    if (s <= 59) {
+        secound.innerHTML = getZero(s)
+    } else{
+        s = 0
+        secound.innerHTML = getZero(s)
+    }
+})
+box4.addEventListener('click', () => {
+    
+})
+box3.addEventListener('click', () => {
+    mm++
+    if (mm <= 99) {
+        millisecound.innerHTML = getZero(mm)
+    } else{
+        mm = 0
+        secound.innerHTML = getZero(mm)
+    }
+})
+box5.addEventListener('click', () => {
+    
 })
